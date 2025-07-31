da2aeb036e5f73a9d7d25b85f3d3bd47e5441e42
Deepika Sisodia <sisodiadeepika28@gmail.com>
2025-07-31 14:06:42 +0530

    Create 1698-ReplaceAllSToAvoidConsecutiveRepeatingCharacters

---
+++ 1698-ReplaceAllSToAvoidConsecutiveRepeatingCharacters/1698-ReplaceAllSToAvoidConsecutiveRepeatingCharacters.java
@@ -0,0 +1,22 @@
+// Last updated: 7/31/2025, 2:06:40 PM
+class Solution {
+    public String modifyString(String s) {
+        if(s==null || s.isEmpty()){
+            return "";
+        }
+
+        char[] chars = s.toCharArray();
+        for(int i=0; i<chars.length; i++){
+            if(chars[i] == '?'){
+                for(char j='a'; j<='z'; j++){
+                    chars[i]=j;
+                    if(i>0 && chars[i-1] == j) continue;
+                    if(i<chars.length-1 && chars[i+1] == j) continue;
+                    break;
+                }
+            }
+        }
+
+        return new String(chars);
+    }
+}
