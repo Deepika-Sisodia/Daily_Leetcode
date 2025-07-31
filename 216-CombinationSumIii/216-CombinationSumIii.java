04bb3347c28bb662f89f7d9eb815a833f865e6e9
Deepika Sisodia <sisodiadeepika28@gmail.com>
2025-07-31 14:09:45 +0530

    Create 216-CombinationSumIii

---
+++ 216-CombinationSumIii/216-CombinationSumIii.java
@@ -0,0 +1,23 @@
+// Last updated: 7/31/2025, 2:09:43 PM
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int[] range = {1,2,3,4,5,6,7,8,9};
+        combination(range,n,k,0,new ArrayList<>(),ans);
+        return ans;
+
+    }
+
+    public static void combination(int[] range, int n, int k, int start, List<Integer> ll,List<List<Integer>> ans){
+        if(n==0 && ll.size()==k){
+            ans.add(new ArrayList<>(ll));
+            return;
+        }
+        
+        for(int i=start; i<range.length; i++){
+            ll.add(range[i]);
+            combination(range,n-range[i],k,i+1,ll,ans);
+            ll.remove(ll.size()-1);
+        }
+    }
+}
