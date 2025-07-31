9bc1a2ee9a81204d34a536bd6240d50417afa512
Deepika Sisodia <sisodiadeepika28@gmail.com>
2025-07-31 14:08:36 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.java
@@ -0,0 +1,20 @@
+// Last updated: 7/31/2025, 2:08:34 PM
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int n = g.length;
+        int m = s.length;
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int l=0, r=0;
+
+        while(l<m && r<n){
+            if(g[r]<=s[l]){
+                r+=1;
+            }
+            l+=1;
+        }
+
+        return r;
+    }
+}
