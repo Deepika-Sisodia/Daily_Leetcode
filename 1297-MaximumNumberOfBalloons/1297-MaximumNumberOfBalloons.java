2a3ad2e8fe1a8050a54c5acbe01e4d1e9bdc482f
Deepika Sisodia <sisodiadeepika28@gmail.com>
2025-07-31 14:07:10 +0530

    Create 1297-MaximumNumberOfBalloons

---
+++ 1297-MaximumNumberOfBalloons/1297-MaximumNumberOfBalloons.java
@@ -0,0 +1,17 @@
+// Last updated: 7/31/2025, 2:07:08 PM
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        HashMap<Character,Integer> count = new HashMap<>();
+        for(char c : text.toCharArray()){
+            count.put(c, count.getOrDefault(c,0)+1);
+        }
+
+        int b = count.getOrDefault('b',0);
+        int a = count.getOrDefault('a',0);
+        int l = count.getOrDefault('l',0)/2;
+        int o = count.getOrDefault('o',0)/2;
+        int n = count.getOrDefault('n',0);
+
+        return Math.min(Math.min(Math.min(b, a), Math.min(l, o)), n);
+    }
+}
