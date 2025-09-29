4111a11300ab221694fd781839eb911dcb2df9d5
Deepika Sisodia <sisodiadeepika28@gmail.com>
2025-09-29 13:32:27 +0530

    Create 1672-RichestCustomerWealth

---
+++ 1672-RichestCustomerWealth/version/java/1672-RichestCustomerWealth_20250929_133225.java
@@ -0,0 +1,27 @@
+// Last updated: 9/29/2025, 1:32:25 PM
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int row=0; row<m; row++){
+            int lo=0; 
+            int hi=n-1;
+            while(lo<=hi){
+                int mid = lo+(hi-lo)/2;
+                if(matrix[row][mid]==target){
+                    return true;
+                }
+                else if(matrix[row][mid]<target){
+                    lo=mid+1;
+
+                }
+                else{
+                    hi=hi-1;
+                }
+            }
+
+        }
+        return false;
+        
+    }
+}
