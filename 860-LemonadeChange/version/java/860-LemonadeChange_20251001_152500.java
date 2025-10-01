659d0dabd547b930c1565169499b3ce31e75594e
Deepika Sisodia <sisodiadeepika28@gmail.com>
2025-10-01 15:25:02 +0530

    Create 860-LemonadeChange

---
+++ 860-LemonadeChange/version/java/860-LemonadeChange_20251001_152500.java
@@ -0,0 +1,36 @@
+// Last updated: 10/1/2025, 3:25:00 PM
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        int twenty = 0;
+        for(int i=0; i<bills.length; i++){
+            if(bills[i]==5){
+                five++;
+            }
+            else if(bills[i]==10){
+                if(five>0){
+                    five--;
+                    ten++;
+                }else{
+                    return false;
+                }
+                
+            }
+            else{
+                if(ten>0 && five>0){
+                    five--;
+                    ten--;
+                }
+                else if(five>=3){
+                    five-=3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
