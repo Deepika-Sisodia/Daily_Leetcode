262036c6f71cb2bf470085bba7ef3b80dd03aef3
Deepika Sisodia <sisodiadeepika28@gmail.com>
2025-10-04 11:15:31 +0530

    Create 344-ReverseString

---
+++ 344-ReverseString/version/java/344-ReverseString_20251004_111530.java
@@ -0,0 +1,16 @@
+// Last updated: 10/4/2025, 11:15:30 AM
+class Solution {
+    public void reverseString(char[] s) {
+        reverse(s,0,s.length-1);
+    }
+
+    public void reverse(char[] s, int start, int end){
+        if(start>end){
+            return;
+        }
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        reverse(s,start+1,end-1);
+    }
+}
