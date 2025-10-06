8fd1276cb31a5edf887140a07d661a837813e479
Deepika Sisodia <sisodiadeepika28@gmail.com>
2025-10-06 13:41:15 +0530

    Create 36-ValidSudoku

---
+++ 36-ValidSudoku/version/java/36-ValidSudoku_20251006_134113.java
@@ -0,0 +1,41 @@
+// Last updated: 10/6/2025, 1:41:13 PM
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(board[i][j]!='.'){
+                    if(!isItValid(board,board[i][j],i,j)){
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isItValid(char[][] board, char target, int row, int col){
+        // for column
+        for(int i=0; i<9; i++){
+            if(i!=row && board[i][col]==target){
+                return false;
+            }
+            if(i!=col && board[row][i]==target){
+                return false;
+            }
+        }
+
+        // for matrix
+        int startrow = (row/3)*3;
+        int startcol = (col/3)*3;
+        for(int i=startrow; i<startrow+3; i++){
+            for(int j=startcol; j<startcol+3; j++){
+                if((i!=row || j!=col) && board[i][j]==target){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
