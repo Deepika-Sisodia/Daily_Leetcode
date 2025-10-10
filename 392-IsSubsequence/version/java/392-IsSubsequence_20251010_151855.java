8cec35430dca142ca83879c95d4f8b7df162a492
Deepika Sisodia <sisodiadeepika28@gmail.com>
2025-10-10 15:18:56 +0530

    Create 392-IsSubsequence

---
+++ 392-IsSubsequence/version/java/392-IsSubsequence_20251010_151855.java
@@ -0,0 +1,21 @@
+// Last updated: 10/10/2025, 3:18:55 PM
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        int c = 0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                c++;
+                i++;
+            }
+            j++;
+        }
+
+        if(c==s.length()){
+            return true;
+        }
+
+        return false;
+    }
+}
