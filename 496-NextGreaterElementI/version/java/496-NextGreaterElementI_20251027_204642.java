9021301a8b73991ec98a00679a0ced5a8eeb92dd
Deepika Sisodia <sisodiadeepika28@gmail.com>
2025-10-27 20:46:44 +0530

    Create 496-NextGreaterElementI

---
+++ 496-NextGreaterElementI/version/java/496-NextGreaterElementI_20251027_204642.java
@@ -0,0 +1,25 @@
+// Last updated: 10/27/2025, 8:46:42 PM
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+
+        for(int i : nums2){
+            while(!st.isEmpty() && st.peek()<i){
+                map.put(st.pop(),i);
+            }
+            st.push(i);
+        }
+
+        while(!st.isEmpty()){
+            map.put(st.pop(),-1);
+        }
+
+        int[] ans = new int[nums1.length];
+        for(int i=0; i<nums1.length; i++){
+            ans[i] = map.get(nums1[i]);
+        }
+
+        return ans;
+    }
+}
